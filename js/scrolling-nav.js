2d3204bec70fd675819d382909720a43411c68d4
AJ Hephner <ajhephner178@gmail.com>
2017-08-29 15:33:21 -0400

    added back javascript i lost on upload

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -2,13 +2,41 @@ $(document).ready(function(){
   alert("JQuery is active")
 
 
+$(".happy").hover(function(e) {
+      e.preventDefault();
+      $(this).stop().fadeOut('slow', function(e) {
+          $('#link').stop().fadeIn('slow');
+      });
+});
+
+$("#link, .happy").mouseout(function(e) {
+      e.preventDefault();
+      if($(this).is("img"))
+      {
+          $(".happy").stop().fadeOut('slow', function(e) {
+              $('#link').stop().fadeOut('slow', function(e) {
+                  $(".happy").stop().fadeIn('slow');
+              });
+          });
+      }
+      else
+      {
+          $('#link').stop().fadeOut('slow', function(e) {
+              $(".happy").stop().fadeIn('slow');
+          });
+      }
+});
+
+$( document ).ready(function() {
+    $(".happy")[0].hover(); 
 });
 
 
-$('.boston').hover(
-    function(){
-         $(this).addClass("lowerOpacity");
-}, function(){
-     $(this).removeClass("lowerOpacity");
+
+
+
 });
+
+
+
 
