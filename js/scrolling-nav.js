dc2e0b5b62a9e48c2279d9f5c228b6878a58fa77
AJ Hephner <ajhephner178@gmail.com>
2017-08-29 19:31:50 -0400

    added images and cleaned up portfolio section

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,41 +1,61 @@
-$(document).ready(function(){
-  alert("JQuery is active")
+            $(document).ready(function(){
+              alert("JQuery is active")
 
 
-$(".happy").hover(function(e) {
-      e.preventDefault();
-      $(this).stop().fadeOut('slow', function(e) {
-          $('#link').stop().fadeIn('slow');
-      });
-});
+            $(".happy").hover(function(e) {
+                  e.preventDefault();
+                  $(this).stop().fadeOut('slow', function(e) {
+                      $('#link').stop().fadeIn('slow');
+                  });
+            });
 
-$("#link, .happy").mouseout(function(e) {
-      e.preventDefault();
-      if($(this).is("img"))
-      {
-          $(".happy").stop().fadeOut('slow', function(e) {
-              $('#link').stop().fadeOut('slow', function(e) {
-                  $(".happy").stop().fadeIn('slow');
-              });
-          });
-      }
-      else
-      {
-          $('#link').stop().fadeOut('slow', function(e) {
-              $(".happy").stop().fadeIn('slow');
-          });
-      }
-});
+            $("#link, .happy").mouseout(function(e) {
+                  e.preventDefault();
+                  if($(this).is("img"))
+                  {
+                      $(".happy").stop().fadeOut('slow', function(e) {
+                          $('#link').stop().fadeOut('slow', function(e) {
+                              $(".happy").stop().fadeIn('slow');
+                          });
+                      });
+                  }
+                  else
+                  {
+                      $('#link').stop().fadeOut('slow', function(e) {
+                          $(".happy").stop().fadeIn('slow');
+                      });
+                  }
+            });
 
-$( document ).ready(function() {
-    $(".happy")[0].hover(); 
-});
+            // $( document ).ready(function() {
+            //     $(".happy")[0].hover(); 
+            // });
+
+            // $("#link1, .screenShot").mouseout(function(e) {
+            //       e.preventDefault();
+            //       if($(this).is("img"))
+            //       {
+            //           $(".screenShot").stop().fadeOut('slow', function(e) {
+            //               $('#link1').stop().fadeOut('slow', function(e) {
+            //                   $(".screenShot").stop().fadeIn('slow');
+            //               });
+            //           });
+            //       }
+            //       else
+            //       {
+            //           $('#link1').stop().fadeOut('slow', function(e) {
+            //               $(".screenShot").stop().fadeIn('slow');
+            //           });
+            //       }
+            // });
+
+            // $( document ).ready(function() {
+            //     $(".screenShot")[0].hover(); 
+            // });
 
 
 
-
-
-});
+            });
 
 
 
