14093e3f35c3408ee2fd6258fc56adcd3f854d39
AJ Hephner <ajhephner178@gmail.com>
2017-11-21 15:20:48 -0500

    change profile pic

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -7,30 +7,31 @@
                 console.log("i was clicked");
           });
 
-            $(".happy").hover(function(e) {
-                  e.preventDefault();
-                  $(this).stop().fadeOut('slow', function(e) {
-                      $('#link').stop().fadeIn('slow');
-                  });
-            });
+            //fades main pic took off when i changed the about me pic
+            // $(".happy").hover(function(e) {
+            //       e.preventDefault();
+            //       $(this).stop().fadeOut('slow', function(e) {
+            //           $('#link').stop().fadeIn('slow');
+            //       });
+            // });
 
-            $("#link, .happy").mouseout(function(e) {
-                  e.preventDefault();
-                  if($(this).is("img"))
-                  {
-                      $(".happy").stop().fadeOut('slow', function(e) {
-                          $('#link').stop().fadeOut('slow', function(e) {
-                              $(".happy").stop().fadeIn('slow');
-                          });
-                      });
-                  }
-                  else
-                  {
-                      $('#link').stop().fadeOut('slow', function(e) {
-                          $(".happy").stop().fadeIn('slow');
-                      });
-                  }
-            });
+            // $("#link, .happy").mouseout(function(e) {
+            //       e.preventDefault();
+            //       if($(this).is("img"))
+            //       {
+            //           $(".happy").stop().fadeOut('slow', function(e) {
+            //               $('#link').stop().fadeOut('slow', function(e) {
+            //                   $(".happy").stop().fadeIn('slow');
+            //               });
+            //           });
+            //       }
+            //       else
+            //       {
+            //           $('#link').stop().fadeOut('slow', function(e) {
+            //               $(".happy").stop().fadeIn('slow');
+            //           });
+            //       }
+            // });
 
        
 
